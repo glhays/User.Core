2c012e2258b74460c561eb76b55d61cdcaae4cd2
Greg Hays <glhays.dev@gmail.com>
2023-09-21 16:16:09 -0700

    INFRA: Initialize Tests Acceptance Project
    Closes: #14

---
+++ User.Core.Tests.Acceptance/Brokers/ApiBroker.Home.cs
@@ -0,0 +1,18 @@
+﻿// -----------------------------------------------------------
+// Copyright(c) Coalition of the Good-Hearted Engineers
+// ======= FREE TO USE FOR THE WORLD =======
+// -----------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace User.Core.Tests.Acceptance.Brokers
+{
+    public partial class ApiBroker
+    {
+        private const string HomeRelativeUrl = "api/home";
+        
+        public async ValueTask<string> GetHomeMessage() =>
+            await this.apiFactoryClient.GetContentStringAsync(HomeRelativeUrl);
+    }
+}
---
+++ User.Core.Tests.Acceptance/Brokers/ApiTestCollection.cs
@@ -0,0 +1,14 @@
+﻿// -----------------------------------------------------------
+// Copyright(c) Coalition of the Good-Hearted Engineers
+// ======= FREE TO USE FOR THE WORLD =======
+// -----------------------------------------------------------
+
+using Xunit;
+
+namespace User.Core.Tests.Acceptance.Brokers
+{
+    [CollectionDefinition(nameof(ApiTestCollection))]
+    public class ApiTestCollection : ICollectionFixture<ApiBroker>
+    {
+    }
+}

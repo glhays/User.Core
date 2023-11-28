81674a2c0bfc722aea25e99e2d3af73a752fc8a4
Greg Hays <glhays.dev@gmail.com>
2023-11-27 17:59:50 -0800

    BROKERS: RoleManagement Add

---
+++ User.Core/Brokers/RoleManagements/RoleManagementBroker.cs
@@ -0,0 +1,40 @@
+﻿// -----------------------------------------------------------
+// Copyright(c) Coalition of the Good-Hearted Engineers
+// ======= FREE TO USE FOR THE WORLD =======
+// -----------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using User.Core.Models.Roles;
+
+namespace User.Core.Brokers.RoleManagements
+{
+    public class RoleManagementBroker : IRoleManagementBroker
+    {
+        public ValueTask<ApplicationRole> InsertRoleAsync(ApplicationRole user)
+        {
+            throw new NotImplementedException();
+        }
+     
+        public IQueryable<ApplicationRole> SelectAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public ValueTask<ApplicationRole> SelectRoleByIdAsync(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<ApplicationRole> UpdateRoleAsync(ApplicationRole user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<ApplicationRole> DeleteRoleAsync(ApplicationRole user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

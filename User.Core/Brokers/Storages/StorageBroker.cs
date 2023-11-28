55f3cd4ef85e57ae4fc68fdae679979cc6fdb75e
Greg Hays <glhays.dev@gmail.com>
2023-11-27 17:31:22 -0800

    BROKERS: Restructure Storages

--- User.Core/Brokers/Storages/StorageBroker.cs
+++ User.Core/Brokers/Storages/StorageBroker.cs
@@ -3,13 +3,19 @@
 // ======= FREE TO USE FOR THE WORLD =======
 // -----------------------------------------------------------
 
-using EFxceptions;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using EFxceptions.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using User.Core.Models.Members;
+using User.Core.Models.Roles;
 
 namespace User.Core.Brokers.Storages
 {
-    public partial class StorageBroker : EFxceptionsContext, IStorageBroker
+    public partial class StorageBroker : EFxceptionsIdentityContext<
+        ApplicationUser, ApplicationRole, Guid>, IStorageBroker
     {
         private readonly IConfiguration configuration;
 
@@ -19,12 +25,41 @@ namespace User.Core.Brokers.Storages
             this.Database.Migrate();
         }
 
+        private async ValueTask<T> InsertAsync<T>(T @object)
+        {
+            this.Entry(@object).State = EntityState.Added;
+            await this.SaveChangesAsync();
+
+            return @object;
+        }
+
+        private IQueryable<T> SelectAll<T>() where T : class => this.Set<T>();
+
+        private async ValueTask<T> SelectAsync<T>(params object[] @objectIds) where T : class =>
+            await this.FindAsync<T>(objectIds);
+
+        private async ValueTask<T> UpdateAsync<T>(T @object)
+        {
+            this.Entry(@object).State = EntityState.Modified;
+            await this.SaveChangesAsync();
+
+            return @object;
+        }
+
+        private async ValueTask<T> DeleteAsync<T>(T @object)
+        {
+            this.Entry(@object).State = EntityState.Deleted;
+            await this.SaveChangesAsync();
+
+            return @object;
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             string connectionString = this.configuration
                 .GetConnectionString(name: "CoreConnection");
 
-            optionsBuilder.UseSqlServer(connectionString: connectionString);            
+            optionsBuilder.UseSqlServer(connectionString: connectionString);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) =>

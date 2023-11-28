6013e2cb489c8a14c234cd9d04c672c1a632bf62
Greg Hays <glhays.dev@gmail.com>
2023-11-27 18:13:12 -0800

    BROKERS: SignInManagement
    Closes: #36

---
+++ User.Core/Brokers/SignInManagements/ISignInManagementBroker.cs
@@ -0,0 +1,22 @@
+﻿// -----------------------------------------------------------
+// Copyright(c) Coalition of the Good-Hearted Engineers
+// ======= FREE TO USE FOR THE WORLD =======
+// -----------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using User.Core.Models.Members;
+
+namespace User.Core.Brokers.SignInManagements
+{
+    public interface ISignInManagementBroker
+    {
+        ValueTask SignOutAsync();
+        
+        ValueTask<SignInResult> PasswordSignInAsync(
+            ApplicationUser user, string password, bool isPersistent, bool lockoutOnFailure);
+        
+        ValueTask<SignInResult> TwoFactorSignInAsync(
+            string provider, string code, bool isPersistent, bool rememberClient);
+    }
+}

a7c38f85d262b9af338f0f79cea396c44caa38e2
Greg Hays <glhays.dev@gmail.com>
2023-11-28 12:54:49 -0800

    BROKERS: UserManagements
    Closes: #40

---
+++ User.Core/Brokers/UserManagements/IUserManagementBroker.cs
@@ -0,0 +1,36 @@
+﻿// -----------------------------------------------------------
+// Copyright(c) Coalition of the Good-Hearted Engineers
+// ======= FREE TO USE FOR THE WORLD =======
+// -----------------------------------------------------------
+
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using User.Core.Models.Members;
+
+namespace User.Core.Brokers.UserManagements
+{
+    public interface IUserManagementBroker
+    {
+        ValueTask<IdentityResult> InsertUserAsync(ApplicationUser user, string password);
+        IQueryable<ApplicationUser> SelectAllUsers();
+        ValueTask<ApplicationUser> SelectUserByIdAsync(Guid userId);
+        ValueTask<ApplicationUser> UpdateUserAsync(ApplicationUser user);
+        ValueTask<ApplicationUser> DeleteUserAsync(ApplicationUser user);
+
+        ValueTask<string> GenerateEmailConfirmationTokenAsync(ApplicationUser user);
+        ValueTask<string> GeneratePasswordResetTokenAsync(ApplicationUser user);
+        ValueTask<string> GenerateTwoFactorTokenAsync(ApplicationUser user);
+        ValueTask<ApplicationUser> FindByNameAsync(string userName);
+        ValueTask<ApplicationUser> FindByEmailAsync(string email);
+        ValueTask<bool> CheckPasswordAsync(ApplicationUser user, string password);
+        ValueTask<IdentityResult> ResetPasswordAsync(ApplicationUser user, string token, string password);
+        ValueTask<IdentityResult> SetTwoFactorEnabledAsync(ApplicationUser user, bool enabled);
+        ValueTask<IList<string>> GetRolesAsync(ApplicationUser user);
+        ValueTask<IdentityResult> AddToRoleAsync(ApplicationUser user, string role);
+        ValueTask<ApplicationUser> FindByIdAsync(string id);
+        ValueTask<IdentityResult> ConfirmEmailAsync(ApplicationUser user, string token);
+    }
+}
---
+++ User.Core/Brokers/UserManagements/UserManagementBroker.cs
@@ -0,0 +1,68 @@
+﻿// -----------------------------------------------------------
+// Copyright(c) Coalition of the Good-Hearted Engineers
+// ======= FREE TO USE FOR THE WORLD =======
+// -----------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using User.Core.Models.Members;
+
+namespace User.Core.Brokers.UserManagements
+{
+    public class UserManagementBroker : IUserManagementBroker
+    {
+        public ValueTask<IdentityResult> InsertUserAsync(ApplicationUser user, string password) =>
+            throw new NotImplementedException();
+
+        public IQueryable<ApplicationUser> SelectAllUsers() => throw new NotImplementedException();
+
+        public ValueTask<ApplicationUser> SelectUserByIdAsync(Guid userId) =>
+            throw new NotImplementedException();
+
+        public ValueTask<ApplicationUser> UpdateUserAsync(ApplicationUser user) =>
+            throw new NotImplementedException();
+
+        public ValueTask<ApplicationUser> DeleteUserAsync(ApplicationUser user) =>
+            throw new NotImplementedException();
+
+        public ValueTask<ApplicationUser> FindByIdAsync(string id) =>
+            throw new NotImplementedException();
+
+        public ValueTask<ApplicationUser> FindByNameAsync(string userName) =>
+            throw new NotImplementedException();
+
+        public ValueTask<IList<string>> GetRolesAsync(ApplicationUser user) =>
+            throw new NotImplementedException();
+
+        public ValueTask<IdentityResult> AddToRoleAsync(ApplicationUser user, string role) =>
+            throw new NotImplementedException();
+
+        public ValueTask<ApplicationUser> FindByEmailAsync(string email) =>
+            throw new NotImplementedException();
+
+        public ValueTask<IdentityResult> ConfirmEmailAsync(ApplicationUser user, string token) =>
+            throw new NotImplementedException();
+
+        public ValueTask<string> GenerateEmailConfirmationTokenAsync(ApplicationUser user) =>
+            throw new NotImplementedException();
+
+        public ValueTask<bool> CheckPasswordAsync(ApplicationUser user, string password) =>
+            throw new NotImplementedException();
+
+        public ValueTask<string> GeneratePasswordResetTokenAsync(ApplicationUser user) =>
+            throw new NotImplementedException();
+
+        public ValueTask<IdentityResult> ResetPasswordAsync(
+            ApplicationUser user, string token, string password) =>
+            throw new NotImplementedException();
+
+        public ValueTask<string> GenerateTwoFactorTokenAsync(ApplicationUser user) =>
+            throw new NotImplementedException();
+
+        public ValueTask<IdentityResult> SetTwoFactorEnabledAsync(
+            ApplicationUser user, bool enabled) => throw new NotImplementedException();
+    }
+}

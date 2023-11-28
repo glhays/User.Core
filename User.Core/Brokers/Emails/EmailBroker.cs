001cbfdb5117727a4ed9430720b60ae812680150
Greg Hays <glhays.dev@gmail.com>
2023-11-27 18:26:07 -0800

    BROKERS: Email
    Closes: #38

---
+++ User.Core/Brokers/Emails/EmailBroker.cs
@@ -0,0 +1,64 @@
+﻿// -----------------------------------------------------------
+// Copyright(c) Coalition of the Good-Hearted Engineers
+// ======= FREE TO USE FOR THE WORLD =======
+// -----------------------------------------------------------
+
+using Microsoft.Extensions.Configuration;
+using RESTFulSense.Clients;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+
+namespace User.Core.Brokers.Emails
+{
+    public class EmailBroker : IEmailBroker
+    {
+        private readonly IConfiguration emailConfigurations;
+        private readonly IRESTFulApiFactoryClient apiClient;
+        private readonly HttpClient httpClient;
+
+        public EmailBroker(IConfiguration configuration)
+        {
+            this.emailConfigurations = configuration;
+            this.httpClient = SetupHttpClient();
+            this.apiClient = SetupApiClient();
+        }
+
+        private async ValueTask<TResult> PostAsync<TRequest, TResult>(string relativeUrl, TRequest content)
+        {
+            return await this.apiClient.PostContentAsync<TRequest, TResult>(
+                relativeUrl,
+                content,
+                mediaType: "application/json",
+                ignoreDefaultValues: true);
+        }
+
+        private async ValueTask<TResult> PostFormAsync<TRequest, TResult>(string relativeUrl, TRequest content)
+            where TRequest : class
+        {
+            return await this.apiClient.PostFormAsync<TRequest, TResult>(
+                relativeUrl,
+                content);
+        }
+
+        private HttpClient SetupHttpClient()
+        {
+            var httpClient = new HttpClient()
+            {
+                BaseAddress =
+                    new Uri(uriString: this.emailConfigurations.GetSection("MailTrap:TestUrl").Value),
+            };
+
+            httpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue(
+                    scheme: "Bearer",
+                    parameter: this.emailConfigurations.GetSection("MailTrap:Token").Value);
+
+            return httpClient;
+        }
+
+        private IRESTFulApiFactoryClient SetupApiClient() =>
+            new RESTFulApiFactoryClient(this.httpClient);
+    }
+}

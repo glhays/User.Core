9d1b325f8e8d2376e681b2a8e917005476762416
Greg Hays <glhays.dev@gmail.com>
2023-11-29 14:30:58 -0800

    ShouldThrowValidationExceptionOnAddIfApplicationUserIsInvalidAndLogItAsync -FAIL

--- User.Core.Tests.Unit/Services/Foundations/Users/ApplicationUserServiceTests.Validations.Add.cs
+++ User.Core.Tests.Unit/Services/Foundations/Users/ApplicationUserServiceTests.Validations.Add.cs
@@ -55,6 +55,91 @@ namespace User.Core.Tests.Unit.Services.Foundations.Users
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
 
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        private async Task ShouldThrowValidationExceptionOnAddIfApplicationUserIsInvalidAndLogItAsync(
+            string invalidText)
+        {
+            // given
+            var invalidApplicationUser = new ApplicationUser
+            {
+                UserName = invalidText,
+            };
+
+            var invalidApplicationUserException =
+                new InvalidApplicationUserException(
+                    message: "Invalid ApplicationUser, correct errors to continue.");
+
+            invalidApplicationUserException.AddData(
+                key: nameof(ApplicationUser),
+                values: "Text is required");
+
+            invalidApplicationUserException.AddData(
+                key: nameof(ApplicationUser.Id),
+                values: "Id is required");
+
+            invalidApplicationUserException.AddData(
+                key: nameof(ApplicationUser.UserName),
+                values: "Text is required");
+
+            invalidApplicationUserException.AddData(
+               key: nameof(ApplicationUser.Email),
+               values: "Text is required");
+
+            invalidApplicationUserException.AddData(
+               key: nameof(ApplicationUser.FirstName),
+               values: "Text is required");
+
+            invalidApplicationUserException.AddData(
+               key: nameof(ApplicationUser.LastName),
+               values: "Text is required");
+
+            invalidApplicationUserException.AddData(
+               key: nameof(ApplicationUser.PhoneNumber),
+               values: "Text is required");
+
+            invalidApplicationUserException.AddData(
+               key: nameof(ApplicationUser.CreatedDate),
+               values: "Date is required");
+
+            invalidApplicationUserException.AddData(
+               key: nameof(ApplicationUser.UpdatedDate),
+               values: "Date is required");
+
+            var expectedApplicationUserValidationException =
+                new ApplicationUserValidationException(
+                    message: "ApplicationUser validation errors occurred, please try again.",
+                    innerException: invalidApplicationUserException);
+
+            // when
+            ValueTask<ApplicationUser> addApplicationUserTask =
+                this.applicationUserService.AddUserAsync(
+                    invalidApplicationUser, GetRandomPassword());
+
+            ApplicationUserValidationException actualApplicationUserValidationException =
+                await Assert.ThrowsAnyAsync<ApplicationUserValidationException>(
+                    addApplicationUserTask.AsTask);
+
+            // then
+            actualApplicationUserValidationException.Should().BeEquivalentTo(
+                expectedApplicationUserValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedApplicationUserValidationException))),
+                    Times.Once());
+
+            this.userManagementBrokerMock.Verify(broker =>
+                broker.InsertUserAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>()),
+                Times.Never());
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.userManagementBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
         [Fact]
         private async Task ShouldThrowValidationExceptionOnAddIfCreatedDateAndUpdatedDateNotTheSameAndLogItAsync()
         {

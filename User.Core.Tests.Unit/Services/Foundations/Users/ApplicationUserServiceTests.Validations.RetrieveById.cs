acdbc025d423e6a7b37eea7eb5576abe4c5829e5
Greg Hays <glhays.dev@gmail.com>
2023-11-30 18:01:45 -0800

    ShouldThrowValidationExceptionOnRetrieveByIdIfApplicationUserIsNotFoundAndLogItAsync -> FAIL

--- User.Core.Tests.Unit/Services/Foundations/Users/ApplicationUserServiceTests.Validations.RetrieveById.cs
+++ User.Core.Tests.Unit/Services/Foundations/Users/ApplicationUserServiceTests.Validations.RetrieveById.cs
@@ -9,6 +9,7 @@ using FluentAssertions;
 using Moq;
 using User.Core.Models.Users;
 using User.Core.Models.Users.Exceptions;
+using Xeptions;
 using Xunit;
 
 namespace User.Core.Tests.Unit.Services.Foundations.Users
@@ -54,6 +55,54 @@ namespace User.Core.Tests.Unit.Services.Foundations.Users
 
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.userManagementBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        private async Task ShouldThrowValidationExceptionOnRetrieveByIdIfApplicationUserIsNotFoundAndLogItAsync()
+        {
+            // given
+            Guid someApplicationUserId = Guid.NewGuid();
+            ApplicationUser nullApplicationUser = null;
+            var innerException = new Exception();
+
+            var notFoundApplicationUserValidationException =
+                new NotFoundApplicationUserException(
+                    message: $"ApplicationUser not found with id: {someApplicationUserId}.",
+                    innerException: innerException.InnerException.As<Xeption>());
+
+            var expectedApplicationUserValidationException =
+                new ApplicationUserValidationException(
+                    message: "User validation error occured, fix error and try again.",
+                    innerException: notFoundApplicationUserValidationException);
+
+            this.userManagementBrokerMock.Setup(broker =>
+                broker.SelectUserByIdAsync(It.IsAny<Guid>()))
+                    .ReturnsAsync(nullApplicationUser);
+
+            // when
+            ValueTask<ApplicationUser> retrieveByIdApplicationUserTask =
+                this.applicationUserService.RetrieveUserByIdAsync(someApplicationUserId);
+
+            ApplicationUserValidationException actualApplicationUserValidationException =
+                await Assert.ThrowsAsync<ApplicationUserValidationException>(
+                    retrieveByIdApplicationUserTask.AsTask);
+
+            // then
+            actualApplicationUserValidationException.Should().BeEquivalentTo(
+                expectedApplicationUserValidationException);
+
+            this.userManagementBrokerMock.Verify(broker =>
+                broker.SelectUserByIdAsync(It.IsAny<Guid>()),
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedApplicationUserValidationException))),
+                        Times.Once());
+
+            this.userManagementBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
     }
---
+++ User.Core/Models/Users/Exceptions/NotFoundApplicationUserException.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------
+// Copyright(c) Coalition of the Good-Hearted Engineers
+// ======= FREE TO USE FOR THE WORLD =======
+// -----------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace User.Core.Models.Users.Exceptions
+{
+    public class NotFoundApplicationUserException : Xeption
+    {
+        public NotFoundApplicationUserException(Guid id)
+                    : base(message: $"ApplicationUser not found with id: {id}.")
+        { }
+
+        public NotFoundApplicationUserException(string message, Xeption innerException)
+            : base(message, innerException)
+        { }
+    }
+}

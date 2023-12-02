df456b3bf97c63923c701bcbb80d4238693d7f40
Greg Hays <glhays.dev@gmail.com>
2023-12-01 16:42:33 -0800

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ User.Core.Tests.Unit/Services/Foundations/Users/ApplicationUserServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,62 @@
+﻿// -----------------------------------------------------------
+// Copyright(c) Coalition of the Good-Hearted Engineers
+// ======= FREE TO USE FOR THE WORLD =======
+// -----------------------------------------------------------
+
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using System;
+using User.Core.Models.Users.Exceptions;
+using Xunit;
+
+namespace User.Core.Tests.Unit.Services.Foundations.Users
+{
+    public partial class ApplicationUserServiceTests
+    {
+        [Fact]
+        private void ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            SqlException sqlException = GetSqlException();
+
+            var failedApplicationUserStorageException =
+                new FailedApplicationUserStorageException(
+                    message: "Failed ApplicationUser storage error occurred, contact support.",
+                    innerException: sqlException);
+
+            var expectedApplicationUserDependencyException =
+                new ApplicationUserDependencyException(
+                    message: "ApplicationUser dependency error occurred, contact support.",
+                    innerException: failedApplicationUserStorageException);
+
+            this.userManagementBrokerMock.Setup(broker =>
+                broker.SelectAllUsers())
+                    .Throws(sqlException);
+
+            // when
+            Action retrieveAllApplicationUsersAction = () =>
+            this.applicationUserService.RetrieveAllUsers();
+
+            ApplicationUserDependencyException actualUserStorageDependencyException =
+                Assert.Throws<ApplicationUserDependencyException>(
+                    retrieveAllApplicationUsersAction);
+
+            // then
+            actualUserStorageDependencyException.Should()
+                .BeEquivalentTo(expectedApplicationUserDependencyException);
+
+            this.userManagementBrokerMock.Verify(broker =>
+                broker.SelectAllUsers(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedApplicationUserDependencyException))),
+                    Times.Once);
+
+            this.userManagementBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

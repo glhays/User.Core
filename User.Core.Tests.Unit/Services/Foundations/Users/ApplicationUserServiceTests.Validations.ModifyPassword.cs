f4273b38ada23e47b5c54a0d4edda8e032be5498
Greg Hays <glhays.dev@gmail.com>
2023-12-20 13:22:17 -0800

    ShouldThrowValidationExceptionOnModifyPasswordIfInvalidAndLogItAsync -> FAIL

--- User.Core.Tests.Unit/Services/Foundations/Users/ApplicationUserServiceTests.Validations.ModifyPassword.cs
+++ User.Core.Tests.Unit/Services/Foundations/Users/ApplicationUserServiceTests.Validations.ModifyPassword.cs
@@ -65,5 +65,64 @@ namespace User.Core.Tests.Unit.Services.Foundations.Users
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.userManagementBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        private async Task ShouldThrowValidationExceptionOnModifyPasswordIfInvalidAndLogItAsync(
+            string invalidText)
+        {
+            // given
+            ApplicationUser randomApplicationUser = CreateRandomApplicationUser();
+            var invalidPassword = invalidText;
+            var invalidToken = invalidText;
+
+            var invalidApplicationUserModifyPasswordException =
+                new InvalidApplicationUserModifyPasswordException(
+                    message: "Invalid modify password occurred, correct errors to continue.");
+
+            invalidApplicationUserModifyPasswordException.AddData(
+                key: nameof(invalidPassword),
+                values: "Text is required");
+            
+            invalidApplicationUserModifyPasswordException.AddData(
+                key: nameof(invalidToken),
+                values: "Text is required");
+
+            var expectedApplicationUserValidationException =
+                new ApplicationUserValidationException(
+                    message: "ApplicationUser validation errors occurred, please try again.",
+                    innerException: invalidApplicationUserModifyPasswordException);
+
+            // when
+            ValueTask<ApplicationUser> applicationUserTask =
+                this.applicationUserService.ModifyUserPasswordAsync(
+                    randomApplicationUser, invalidToken, invalidPassword);
+
+            ApplicationUserValidationException actualUserValidationException =
+              await Assert.ThrowsAsync<ApplicationUserValidationException>(
+                  applicationUserTask.AsTask);
+
+            // then
+            actualUserValidationException.Should().BeEquivalentTo(
+                expectedApplicationUserValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedApplicationUserValidationException))),
+                        Times.Once);
+
+            this.userManagementBrokerMock.Verify(broker =>
+                broker.UpdateUserPasswordAsync(
+                    It.IsAny<ApplicationUser>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>()),
+                    Times.Never);
+
+            this.userManagementBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
---
+++ User.Core/Models/Users/Exceptions/InvalidApplicationUserModifyPasswordException.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------
+// Copyright(c) Coalition of the Good-Hearted Engineers
+// ======= FREE TO USE FOR THE WORLD =======
+// -----------------------------------------------------------
+
+using Xeptions;
+
+namespace User.Core.Models.Users.Exceptions
+{
+    public class InvalidApplicationUserModifyPasswordException : Xeption
+    {
+        public InvalidApplicationUserModifyPasswordException()
+            : base(message: "Invalid modify password occurred, correct errors to continue.")
+        { }
+
+        public InvalidApplicationUserModifyPasswordException(string message)
+            : base(message)
+        { }
+    }
+}

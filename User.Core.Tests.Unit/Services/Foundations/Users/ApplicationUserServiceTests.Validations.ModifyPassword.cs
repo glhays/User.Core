cc2f17f979f913d261828bd6b342d84e414cb15d
Greg Hays <glhays.dev@gmail.com>
2023-12-20 11:28:26 -0800

    ShouldThrowValidationExceptionOnModifyUserPasswordIfUserIsNullAndLogItAsync -> FAIL

---
+++ User.Core.Tests.Unit/Services/Foundations/Users/ApplicationUserServiceTests.Validations.ModifyPassword.cs
@@ -0,0 +1,69 @@
+﻿// -----------------------------------------------------------
+// Copyright(c) Coalition of the Good-Hearted Engineers
+// ======= FREE TO USE FOR THE WORLD =======
+// -----------------------------------------------------------
+
+using FluentAssertions;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using User.Core.Models.Users;
+using User.Core.Models.Users.Exceptions;
+using Xunit;
+
+namespace User.Core.Tests.Unit.Services.Foundations.Users
+{
+    public partial class ApplicationUserServiceTests
+    {
+        [Fact]
+        private async void ShouldThrowValidationExceptionOnModifyUserPasswordIfUserIsNullAndLogItAsync()
+        {
+            // given
+            ApplicationUser invalidApplicationUser = null;
+            string randomToken = GetRandomWord();
+            string inputToken = randomToken;
+            string randomPassword = GetRandomPassword();
+            string inputPassword = randomPassword;
+            var innerException = new Exception();
+
+            var nullApplicationUserException = 
+                new NullApplicationUserException(
+                    message: "ApplicationUser is null, please fix and try again.",
+                    innerException: innerException);
+
+            var expectedApplicationUserValidationException =
+                new ApplicationUserValidationException(
+                    message: "ApplicationUser validation errors occurred, please try again.",
+                    innerException: nullApplicationUserException);
+
+            // when
+            ValueTask<ApplicationUser> applicationUserTask =
+                this.applicationUserService.ModifyUserPasswordAsync(
+                    invalidApplicationUser, inputToken, inputPassword);
+
+            ApplicationUserValidationException actualUserValidationException =
+              await Assert.ThrowsAsync<ApplicationUserValidationException>(
+                  applicationUserTask.AsTask);
+
+            // then
+            actualUserValidationException.Should().BeEquivalentTo(
+                expectedApplicationUserValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedApplicationUserValidationException))),
+                        Times.Once);
+
+            this.userManagementBrokerMock.Verify(broker =>
+                broker.UpdateUserPasswordAsync(
+                    It.IsAny<ApplicationUser>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.userManagementBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

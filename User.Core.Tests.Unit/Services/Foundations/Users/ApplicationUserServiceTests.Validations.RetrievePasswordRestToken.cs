33a8b40c7e8a431fa0737d2b8d6985ae5416b860
Greg Hays <glhays.dev@gmail.com>
2023-12-21 13:56:41 -0800

    ShouldThrowValidationExceptionOnRetrievePasswordResetTokenIfUserIsNullAndLogItAsync -> PASS

---
+++ User.Core.Tests.Unit/Services/Foundations/Users/ApplicationUserServiceTests.Validations.RetrievePasswordRestToken.cs
@@ -0,0 +1,64 @@
+﻿// -----------------------------------------------------------
+// Copyright(c) Coalition of the Good-Hearted Engineers
+// ======= FREE TO USE FOR THE WORLD =======
+// -----------------------------------------------------------
+
+using FluentAssertions;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using User.Core.Models.Users;
+using User.Core.Models.Users.Exceptions;
+using Xunit;
+
+namespace User.Core.Tests.Unit.Services.Foundations.Users
+{
+    public partial class ApplicationUserServiceTests
+    {
+        [Fact]
+        private async void ShouldThrowValidationExceptionOnRetrievePasswordResetTokenIfUserIsNullAndLogItAsync()
+        {
+            // given
+            ApplicationUser invalidApplicationUser = null;
+            ApplicationUser inputApplicationUser = invalidApplicationUser;
+            var innerException = new Exception();
+
+            var nullApplicationUserException = 
+                new NullApplicationUserException(
+                    message: "ApplicationUser is null, please fix and try again.",
+                    innerException: innerException);
+
+            var expectedApplicationUserValidationException =
+                new ApplicationUserValidationException(
+                    message: "ApplicationUser validation errors occurred, please try again.",
+                    innerException: nullApplicationUserException);
+
+            // when
+            ValueTask<string> applicationUserPasswordResetTokenTask =
+                this.applicationUserService.RetrieveUserPasswordResetTokenAsync(
+                    inputApplicationUser);
+
+            ApplicationUserValidationException actualUserValidationException =
+              await Assert.ThrowsAsync<ApplicationUserValidationException>(
+                  applicationUserPasswordResetTokenTask.AsTask);
+
+            // then
+            actualUserValidationException.Should().BeEquivalentTo(
+                expectedApplicationUserValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedApplicationUserValidationException))),
+                        Times.Once);
+
+            this.userManagementBrokerMock.Verify(broker =>
+                broker.SelectUserPasswordResetTokenAsync(
+                    It.IsAny<ApplicationUser>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.userManagementBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

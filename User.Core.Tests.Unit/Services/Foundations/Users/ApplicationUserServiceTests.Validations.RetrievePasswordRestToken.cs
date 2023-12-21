0b00a6f571033fca726c30446ed59ae3e14ceb16
Greg Hays <glhays.dev@gmail.com>
2023-12-21 14:54:25 -0800

    ShouldThrowValidationExceptionOnRetrievePasswordResetTokenIfUserInvalidAndLogItAsync -> PASS

--- User.Core.Tests.Unit/Services/Foundations/Users/ApplicationUserServiceTests.Validations.RetrievePasswordRestToken.cs
+++ User.Core.Tests.Unit/Services/Foundations/Users/ApplicationUserServiceTests.Validations.RetrievePasswordRestToken.cs
@@ -60,5 +60,85 @@ namespace User.Core.Tests.Unit.Services.Foundations.Users
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.userManagementBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        private async Task ShouldThrowValidationExceptionOnRetrievePasswordResetTokenIfUserInvalidAndLogItAsync(
+            string invalidText)
+        {
+            // given
+            var invalidApplicationUser = new ApplicationUser
+            {
+                FirstName = invalidText,
+                LastName = invalidText
+            };
+
+            var invalidApplicationUserException =
+                new InvalidApplicationUserException(
+                    message: "Invalid ApplicationUser, correct errors to continue.");
+
+            invalidApplicationUserException.AddData(
+                key: nameof(ApplicationUser.FirstName),
+                values: "Text is required");
+
+            invalidApplicationUserException.AddData(
+                key: nameof(ApplicationUser.LastName),
+                values: "Text is required");
+
+            invalidApplicationUserException.AddData(
+                key: nameof(ApplicationUser.UserName),
+                values: "Text is required");
+
+            invalidApplicationUserException.AddData(
+                key: nameof(ApplicationUser.PhoneNumber),
+                values: "Text is required");
+
+            invalidApplicationUserException.AddData(
+                key: nameof(ApplicationUser.Email),
+                values: "Text is required");
+
+            invalidApplicationUserException.AddData(
+                key: nameof(ApplicationUser.CreatedDate),
+                values: "Date is required");
+
+            invalidApplicationUserException.AddData(
+                key: nameof(ApplicationUser.UpdatedDate),
+                values: "Date is required");
+
+            var expectedApplicationUserValidationException =
+                new ApplicationUserValidationException(
+                    message: "ApplicationUser validation errors occurred, please try again.",
+                    innerException: invalidApplicationUserException);
+
+            // when
+            ValueTask<string> applicationUserPasswordResetTokenTask =
+                this.applicationUserService.RetrieveUserPasswordResetTokenAsync(
+                    invalidApplicationUser);
+
+            ApplicationUserValidationException actualUserValidationException =
+              await Assert.ThrowsAsync<ApplicationUserValidationException>(
+                  applicationUserPasswordResetTokenTask.AsTask);
+
+            // then
+            actualUserValidationException.Should().BeEquivalentTo(
+                expectedApplicationUserValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedApplicationUserValidationException))),
+                        Times.Once);
+
+            this.userManagementBrokerMock.Verify(broker =>
+                broker.SelectUserPasswordResetTokenAsync(
+                    It.IsAny<ApplicationUser>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.userManagementBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
     }
 }
